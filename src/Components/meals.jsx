22f0d175edadf2560c3dd0ed6c7706ff28f58284
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-06-30 21:17:18 +0100

    Child component state management done

--- src/Components/meals.jsx
+++ src/Components/meals.jsx
@@ -1,23 +1,25 @@
 import { use, useState } from "react"
 import Meal from "./meal";
-export default function Meals({promiseFromApi}){
+export default function Meals({ promiseFromApi }) {
     // console.log(promiseFromApi);
-    const [orderedFood,setOrderedFoods] = useState([]);
-    const handleOrderedItems=(meal)=>{
-        console.log("ordered items clicked",meal);
-    }
+    const [orderedFood, setOrderedFoods] = useState([]);
 
+    const handleOrderedItems = (meal) => {
+        console.log("ordered items clicked", meal);
+        const newOrderedItems = [...orderedFood,meal];
+        setOrderedFoods(newOrderedItems);
+    }
     const foods = use(promiseFromApi);
     console.log(foods);
-    return(
+    return (
         <div>
-             <h1>MyArray length:{foods.meals.length}</h1>
-             <h2>How Many times Ordered: </h2>
-             {
-                foods.meals.map(meal=><Meal 
+            <h1>MyArray length:{foods.meals.length}</h1>
+            <h2>How Many times Ordered: {orderedFood.length}</h2>
+            {
+                foods.meals.map(meal => <Meal
                     handleOrderedItems={handleOrderedItems}
-                    key={meal.idMeal} meal={meal}></Meal> )
-             }
+                    key={meal.idMeal} meal={meal}></Meal>)
+            }
         </div>
     )
 }
